7c6dba9f4417185933874123d40c7ee60322011f
Kaisar Butin <bolawak@gmail.com>
2017-05-22 17:23:06 +0600

    Initial commit

---
+++ src/kz/kaisar/www/Location.java
@@ -0,0 +1,36 @@
+package kz.kaisar.www;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Kaisar on 5/15/17.
+ */
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<>();
+        this.exits.put("Q", 0);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+
+    public void addExit(String direction, int location) {
+        exits.put(direction, location);
+    }
+}
